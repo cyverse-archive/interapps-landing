e4cd71c111c880a6168dc5a3f9aef29995a084bd
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-05-29 14:15:24 -0700

    Begin work to enforce permissions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -9,6 +10,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"path"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -42,6 +44,10 @@ type CASProxy struct {
 	frontendURL  string // The URL placed into service query param for CAS.
 	backendURL   string // The backend URL to forward to.
 	wsbackendURL string // The websocket URL to forward requests to.
+	resourceType string // The resource type for analysis.
+	resourceName string // The UUID of the analysis.
+	permsURL     string // The service URL for the permissions service.
+	subjectType  string // The subject type for a user.
 }
 
 // NewCASProxy returns a newly instantiated *CASProxy.
@@ -53,6 +59,122 @@ func NewCASProxy(casBase, casValidate, frontendURL, backendURL, wsbackendURL str
 		backendURL:   backendURL,
 		wsbackendURL: wsbackendURL,
 	}
+}
+
+// Analysis contains the ID for the Analysis, which gets used as the resource
+// name when checking permissions.
+type Analysis struct {
+	ID string `json:"id"` // Literally all we care about here.
+}
+
+// Analyses is a list of analyses returned by the apps service.
+type Analyses struct {
+	Analyses []Analysis `json:"analyses"`
+}
+
+func getResourceName(appsURL, appsUser, externalID string) (string, error) {
+	reqURL, err := url.Parse(appsURL)
+	if err != nil {
+		return "", err
+	}
+	reqURL.Path = filepath.Join(reqURL.Path, "admin/analyses/by-external-id", externalID)
+
+	v := url.Values{}
+	v.Set("user", appsUser)
+	reqURL.RawQuery = v.Encode()
+
+	resp, err := http.Get(reqURL.String())
+	defer func() {
+		if resp != nil {
+			if resp.Body != nil {
+				resp.Body.Close()
+			}
+		}
+	}()
+	if err != nil {
+		return "", err
+	}
+
+	analyses := &Analyses{}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if err = json.Unmarshal(b, analyses); err != nil {
+		return "", err
+	}
+	if len(analyses.Analyses) < 1 {
+		return "", errors.New("no analyses found")
+	}
+	return analyses.Analyses[0].ID, nil
+}
+
+// Resource is an item that can have permissions attached to it in the
+// permissions service.
+type Resource struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"resource_type"`
+}
+
+// Subject is an item that accesses resources contained in the permissions
+// service.
+type Subject struct {
+	ID        string `json:"id"`
+	SubjectID string `json:"subject_id"`
+	SourceID  string `json:"subject_source_id"`
+	Type      string `json:"subject_type"`
+}
+
+// Permission is an entry from the permissions service that tells what access
+// a subject has to a resource.
+type Permission struct {
+	ID       string   `json:"id"`
+	Level    string   `json:"permission_level"`
+	Resource Resource `json:"resource"`
+	Subject  Subject  `json:"subject"`
+}
+
+// PermissionList contains a list of permission returned by the permissions
+// service.
+type PermissionList struct {
+	Permissions []Permission `json:"permissions"`
+}
+
+// IsAllowed will return true if the user is allowed to access the running app
+// and false if they're not. An error might be returned as well. Access should
+// be denied if an error is returned, even if the boolean return value is true.
+func (c *CASProxy) IsAllowed(user string) (bool, error) {
+	requrl, err := url.Parse(c.permsURL)
+	if err != nil {
+		return false, err
+	}
+	requrl.Path = filepath.Join(requrl.Path, "permissions/subjects", c.subjectType, user, c.resourceType, c.resourceName)
+	resp, err := http.Get(requrl.String())
+	defer func() {
+		if resp != nil {
+			if resp.Body != nil {
+				resp.Body.Close()
+			}
+		}
+	}()
+	if err != nil {
+		return false, err
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+	l := &PermissionList{}
+	if err = json.Unmarshal(b, l); err != nil {
+		return false, err
+	}
+	if len(l.Permissions) > 0 {
+		if l.Permissions[0].Level != "" {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 // ValidateTicket will validate a CAS ticket against the configured CAS server.
@@ -112,6 +234,8 @@ func (c *CASProxy) ValidateTicket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
+
+	log.Println(resp.Header)
 
 	// This is where the actual ticket validation happens. If the CAS server
 	// returns 'no\n\n' in the body, then the validation was not successful. The
@@ -247,6 +371,7 @@ func (c *CASProxy) Proxy() (http.Handler, error) {
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%+v\n", r.Header)
 		if c.isWebsocket(r) {
 			ws.ServeHTTP(w, r)
 			return
@@ -266,6 +391,12 @@ func main() {
 		maxAge       = flag.Int("max-age", 0, "The idle timeout for session, in seconds.")
 		sslCert      = flag.String("ssl-cert", "", "Path to the SSL .crt file.")
 		sslKey       = flag.String("ssl-key", "", "Path to the SSL .key file.")
+		resourceType = flag.String("resource-type", "analysis", "The resource type that gets passed to the permissions service.")
+		permsURL     = flag.String("permissions-url", "", "The URL for the permissions service.")
+		subjectType  = flag.String("subject-type", "user", "The subject type to pass to the permissions service.")
+		appsUser     = flag.String("apps-user", "", "Username to use when calling the apps api.")
+		appsURL      = flag.String("apps-url", "", "The URL for the apps service.")
+		externalID   = flag.String("external-id", "", "The external ID to pass to the apps service when looking up the analysis ID.")
 	)
 
 	flag.Parse()
@@ -299,6 +430,22 @@ func main() {
 		*wsbackendURL = w.String()
 	}
 
+	if *appsUser == "" {
+		log.Fatal("--apps-user must be set.")
+	}
+
+	if *appsURL == "" {
+		log.Fatal("--apps-url must be set.")
+	}
+
+	if *permsURL == "" {
+		log.Fatal("--permissions-url must be set.")
+	}
+
+	if *externalID == "" {
+		log.Fatal("--external-id must be set.")
+	}
+
 	log.Infof("backend URL is %s", *backendURL)
 	log.Infof("websocket backend URL is %s", *wsbackendURL)
 	log.Infof("frontend URL is %s", *frontendURL)
@@ -306,7 +453,22 @@ func main() {
 	log.Infof("CAS base URL is %s", *casBase)
 	log.Infof("CAS ticket validator endpoint is %s", *casValidate)
 
-	p := NewCASProxy(*casBase, *casValidate, *frontendURL, *backendURL, *wsbackendURL)
+	resourceName, err := getResourceName(*appsURL, *appsUser, *externalID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p := &CASProxy{
+		casBase:      *casBase,
+		casValidate:  *casValidate,
+		frontendURL:  *frontendURL,
+		backendURL:   *backendURL,
+		wsbackendURL: *wsbackendURL,
+		resourceType: *resourceType,
+		subjectType:  *subjectType,
+		resourceName: resourceName,
+		permsURL:     *permsURL,
+	}
 
 	proxy, err := p.Proxy()
 	if err != nil {
