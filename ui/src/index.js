d929f7d7a697f9b7132af746a7cae6aadb3243d0
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-09-26 15:50:21 -0700

    Add ui/src/[images,components,css] and move files into them

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
