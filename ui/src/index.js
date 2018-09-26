df761ac7220829ab140096edfe2bdc14f773a06c
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-09-26 15:52:36 -0700

    Remove unused imports

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
