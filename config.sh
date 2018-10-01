d15b0334e09089df271841d84da689562d4a1a5a
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-10-01 16:04:17 -0700

    Allow starting the entire app from the top-level dir. Some config options are still evil, but it's working

---
+++ config.sh
@@ -0,0 +1,5 @@
+export NPM_CONFIG_API_HOST=localhost:8082
+export NPM_CONFIG_CAS_URL=http://hana.iplantcollaborative.org/cas/
+export NPM_CONFIG_GRAPHQL_HOSTNAME=localhost
+export NPM_CONFIG_GRAPHQL_PORT=8081
+export NPM_CONFIG_INGRESS_URL=http://quixote.cyverse.org:31377
