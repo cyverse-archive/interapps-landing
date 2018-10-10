57b147d74b5cf442b932707c41ccc0ec5438360c
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-10-10 12:44:55 -0700

    Switch to build with relative pathing by setting the homepage property to '.'. Fixed the start-server.js script

--- start-server.js
+++ start-server.js
@@ -2,11 +2,10 @@ const args = [
   'run',
   'main.go',
   `--cas-base-url=${process.env.NPM_CONFIG_CAS_URL}`,
-  '--disable-custom-header-match',
   `--graphql=http://${process.env.NPM_CONFIG_GRAPHQL_HOSTNAME}:${process.env.NPM_CONFIG_GRAPHQL_PORT}/v1alpha1/graphql`,
   `--ingress-url=${process.env.NPM_CONFIG_INGRESS_URL}`,
   `--listen-addr=${process.env.NPM_CONFIG_API_HOST}`,
-	`--vice-base-url=${process.env.NPM_CONFIG_VICE_URL}`
+  `--vice-base-url=${process.env.NPM_CONFIG_VICE_URL}`
 ];
 
 const opts = {
