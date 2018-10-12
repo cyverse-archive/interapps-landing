222329dd7f4231c24d6642e94bc30aea412d8043
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-10-12 08:55:46 -0700

    Initial work on the app bar for the landing page

---
+++ project-ui-landing/src/components/appbar.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class LandingAppBar extends Component {
+  state = {
+    anchorEl: null
+  };
+
+  // Called when the menu button is clicked.
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  // Called when the menu is closed.
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.MenuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Visual Interactive Computing Environment
+            </Typography>
+
+            <div>
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>My Account</MenuItem>
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+
+LandingAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LandingAppBar);
--- project-ui-landing/src/stories/index.js
+++ project-ui-landing/src/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Button from '@material-ui/core/Button';
+import LandingAppBar from '../components/appbar';
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -10,3 +11,8 @@ storiesOf('Button', module)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
   ));
+
+storiesOf('LandingAppBar', module)
+  .add('default', () => (
+    <LandingAppBar />
+  ))
