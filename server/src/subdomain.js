370195de17c3ae678f699f2fd3cb36850749e6b7
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-12-12 09:17:15 -0700

    Add debug logging using the debug module

--- server/src/subdomain.js
+++ server/src/subdomain.js
@@ -1,3 +1,7 @@
+import url from 'url';
+
+const debug = require('debug')('subdomain');
+
 // escapeRegExp will escape a string so that it can be safely interpolated into
 // a regular expression string as a literal string. Taken from:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
@@ -13,13 +17,13 @@ function escapeRegExp(string) {
 // a subdomain is included in the string it's applied to. Uses the VICE_DOMAIN
 // environment variable.
 function getSubdomainRegex() {
-  const viceURL = new URL(process.env.VICE_DOMAIN);
+  const viceURL = new url.URL(process.env.VICE_DOMAIN);
   const viceDomain = escapeRegExp(viceURL.host);
   return new RegExp(`(a.*\.)?${viceDomain}(:[0-9]+)?`);
 }
 
 export function extractSubdomain(urlWithSubdomain) {
-  const u = new URL(urlWithSubdomain);
+  const u = new url.URL(urlWithSubdomain);
   const fields = u.hostname.split(".");
   if (fields.length < 2) {
     throw new Error(`no subdomain found in ${urlWithSubdomain}`);
@@ -37,8 +41,12 @@ export function extractSubdomain(urlWithSubdomain) {
 // and is part of the configured VICE_DOMAIN.
 export default function hasValidSubdomain(str) {
   const fields = getSubdomainRegex().exec(str);
-  return fields !== null &&
-         fields.length >= 2 &&
-         fields[0] !== undefined &&
-         fields[1] !== undefined;
+  const isValid = (
+    fields !== null &&
+    fields.length >= 2 &&
+    fields[0] !== undefined &&
+    fields[1] !== undefined
+  );
+  debug(`hasValidSubdomain; input: ${str}; result: ${isValid}`);
+  return isValid;
 }
