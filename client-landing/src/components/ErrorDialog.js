9d152884a1dacfc1256cdff28ff986dd689460a2
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-01-31 10:32:53 -0700

    Error dialog manages a queue of errors

--- client-landing/src/components/ErrorDialog.js
+++ client-landing/src/components/ErrorDialog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { clearErrorsByStatus, setErrorDialogOpen } from '../actions';
+import { clearErrors, setErrorDialogOpen, rmError } from '../actions';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
+
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -10,12 +12,36 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Divider from '@material-ui/core/Divider';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Fade from '@material-ui/core/Fade';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ErrorOutline from '@material-ui/icons/ErrorOutline';
+import FileCopy from '@material-ui/icons/FileCopy';
+import RemoveCircle from '@material-ui/icons/RemoveCircle';
 
-const styles = theme => ({});
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  errorIcon: {
+    position: 'relative',
+    top: '4px',
+    paddingRight: '10px',
+  },
+});
 
 class ErrorDialog extends Component {
   render() {
-    const { classes, open, error, handleClose, handleClear } = this.props;
+    const { classes, open, errors, handleClose, handleClear, handleRemove } = this.props;
 
     return (
       <div>
@@ -25,13 +51,43 @@ class ErrorDialog extends Component {
           aria-labelledby="error-dialog-title"
           aria-describedby="error-dialog-description"
         >
-          <DialogTitle id="error-dialog-title">{"Errors"}</DialogTitle>
+          <DialogTitle id="error-dialog-title">
+            <ErrorOutline className={classes.errorIcon} />
+            Errors
+          </DialogTitle>
           <DialogContent>
-            <DialogContentText id="error-dialog-description">
-              Placeholder Text
-            </DialogContentText>
+            {
+              (errors.length > 0) ?
+                errors.map((error, index) => {
+                  const displayDate = new Date(error.dateCreated);
+                  const displayDateString = `${displayDate.toLocaleDateString()} ${displayDate.toLocaleTimeString()}`;
+
+                  return (
+                    <ExpansionPanel id={`expansion-panel-${index}`}>
+                      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography className={classes.heading}>{error.status} Error - {displayDateString}</Typography>
+                      </ExpansionPanelSummary>
+                      <ExpansionPanelDetails>
+                        <Typography>{error.message}</Typography>
+                      </ExpansionPanelDetails>
+
+                      <ExpansionPanelActions>
+                        <CopyToClipboard text={error.message}>
+                          <Button size="small"><FileCopy /></Button>
+                        </CopyToClipboard>
+                        <Button size="small" onClick={() => handleRemove(index)}><RemoveCircle /></Button>
+                      </ExpansionPanelActions>
+                    </ExpansionPanel>
+                  );
+                })
+                :
+                <Typography>No errors have been received.</Typography>
+            }
           </DialogContent>
           <DialogActions>
+            <Button onClick={handleClear} color="primary">
+              Clear
+            </Button>
             <Button onClick={handleClose} color="primary">
               Close
             </Button>
@@ -44,18 +100,19 @@ class ErrorDialog extends Component {
 
 ErrorDialog.propTypes = {
   classes: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired,
-  open: PropTypes.bool.isRequired,
+  errors:  PropTypes.array.isRequired,
+  open:    PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   errors: state.errors,
-  open: state.errorDialogOpen,
+  open:   state.errorDialogOpen,
 });
 
 const mapDispatchToProps = dispatch => ({
-  handleClose: () => dispatch(setErrorDialogOpen(false)),
-  handleClear: (statusCode) => dispatch(clearErrorsByStatus(statusCode)),
+  handleClose:  () => dispatch(setErrorDialogOpen(false)),
+  handleRemove: (index) => dispatch(rmError(index)),
+  handleClear:  () => dispatch(clearErrors()),
 });
 
 const MappedErrorDialog = connect(
